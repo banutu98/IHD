e8e4cc9a6a451591314a43b1f0bad9af87ba6bdd
banutu98 <serbanstol@yahoo.com>
2019-10-12 23:03:29 +0200

    Partitioned dataset resolves #2

--- utilities/utils.py
+++ utilities/utils.py
@@ -1,7 +1,8 @@
 import os
 import pandas as pd
 
-def get_csv_train(data_prefix='./data', verbose=False):
+
+def get_csv_train(data_prefix='../data'):
     train_df = pd.read_csv(os.path.join(data_prefix, 'stage_1_train.csv'))
     train_df[['ID', 'subtype']] = train_df['ID'].str.rsplit('_', 1,
                                                             expand=True)
@@ -9,3 +10,15 @@ def get_csv_train(data_prefix='./data', verbose=False):
     train_df = pd.pivot_table(train_df, index='id',
                               columns='subtype', values='label')
     return train_df
+
+
+def extract_csv_partition():
+    df = get_csv_train()
+    negative, positive = df.loc[df['any'] == 0], df.loc[df['any'] == 1]
+    negative = negative.sample(n=len(positive.index))
+    return pd.concat([positive, negative])
+
+
+if __name__ == '__main__':
+    partition = extract_csv_partition()
+    print(partition.index.values)
