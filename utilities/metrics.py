46c27f45cfe553f1508d8190181b2486601ef0f2
banutu98 <serbanstol@yahoo.com>
2019-10-17 22:39:27 +0200

    added loss metric + resolves #9 + resolves #10 + resolves #11 + resolves #14

---
+++ utilities/metrics.py
@@ -0,0 +1,11 @@
+import numpy as np
+from keras import backend as K
+
+
+def weighted_log_loss(y_true, y_pred):
+    class_weights = np.array([2., 1., 1., 1., 1., 1.])
+    eps = K.epsilon()
+    y_pred = K.clip(y_pred, eps, 1.0 - eps)
+    out = -(y_true * K.log(y_pred) * class_weights +
+            (1.0 - y_true) * K.log(1.0 - y_pred) * class_weights)
+    return K.mean(out, axis=-1)
